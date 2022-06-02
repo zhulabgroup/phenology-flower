5e54761d17a26d65bb91d581d884b55faff3d306
yiluansong <yiluansong@gmail.com>
2022-06-02 03:38:36 -0700

    shiny app

--- _deploy.sh
+++ _deploy.sh
@@ -2,7 +2,8 @@
 
 set -ev
 
-ssh root@zhulab.ucsc.edu "rm -rf ~/wordpress/projects/RS4flower/*"
+/opt/microsoft/ropen/4.0.2/lib64/R/bin/Rscript -e "bookdown::publish_book(render = 'local',account = 'yiluansong', server = 'bookdown.org')"
+# ssh root@zhulab.ucsc.edu "rm -rf ~/wordpress/projects/RS4flower/*"
 
-cp -r figures/ _book/
-scp -r _book/* root@zhulab.ucsc.edu:~/wordpress/projects/RS4flower/
+# cp -r figures/ _book/
+# scp -r _book/* root@zhulab.ucsc.edu:~/wordpress/projects/RS4flower/
