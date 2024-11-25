9dcfb2d46c84162d8d378e150d2fca18e71e4027
yiluansong <yiluansong@gmail.com>
2024-11-24 19:32:04 -0500

    add taxa, style

--- code/prep_scope.R
+++ code/prep_scope.R
@@ -11,24 +11,31 @@ v_taxa <- c(
   # "Poaceae early",
   # "Poaceae late",
   "Acer",
-  "Populus"
+  "Populus",
+  "Juglans",
+  "Platanus",
+  "Alnus",
+  "Carya",
+  "Liquidambar",
+  "Salix",
+  "Celtis"
 )
 v_taxa_short <- str_split(v_taxa, pattern = " ", simplify = T)[, 1]
 # taxa in chronological order
-v_taxa_chron <- c( # "Cupressaceae",
-  "Fraxinus",
-  "Ulmus early",
-  # "Pinaceae",
-  "Acer",
-  "Populus",
-  "Quercus",
-  "Betula",
-  "Morus",
-  # "Poaceae early",
-  # "Poaceae late",
-  "Ulmus late" # ,
-  # "Ambrosia"
-)
+# v_taxa_chron <- c( # "Cupressaceae",
+#   "Fraxinus",
+#   "Ulmus early",
+#   # "Pinaceae",
+#   "Acer",
+#   "Populus",
+#   "Quercus",
+#   "Betula",
+#   "Morus",
+#   # "Poaceae early",
+#   # "Poaceae late",
+#   "Ulmus late" ,
+#   # "Ambrosia"
+# )
 
 v_site <- c(
   "NY",
