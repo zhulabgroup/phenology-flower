0815c6ff8f56f36138b9f4ee398bdf26239955e3
yiluansong <44077975+yiluansong@users.noreply.github.com>
2025-02-11 20:54:42 -0500

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,4 @@
 set -ev
 
 /sw/pkgs/arc/stacks/gcc/10.3.0/R/4.2.0/bin/R -e "bookdown::publish_book(render = 'local',account = 'yiluansong', server = 'bookdown.org')"
-# ssh root@zhulab.ucsc.edu "rm -rf ~/wordpress/projects/RS4flower/*"
 
-# cp -r figures/ _book/
-# scp -r _book/* root@zhulab.ucsc.edu:~/wordpress/projects/RS4flower/
