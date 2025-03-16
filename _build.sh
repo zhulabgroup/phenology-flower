9202dae3938ad5607344be74f1f0eb0fd620ef35
yiluansong <yiluansong@gmail.com>
2025-03-15 21:19:44 -0400

    delete bookdown structure

--- _build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -ev
-
-/sw/pkgs/arc/stacks/gcc/10.3.0/R/4.2.0/bin/R -e "rmarkdown::clean_site(preview = FALSE)"
-/sw/pkgs/arc/stacks/gcc/10.3.0/R/4.2.0/bin/R -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
